fca0393185706ca0c92274a6e1fe7d8c598821fc
Liorlevy180 <40452442+Liorlevy180@users.noreply.github.com>
2018-07-06 04:16:44 +0300

    Add files via upload

---
+++ rand/rand/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r;
+            Console.WriteLine("press anything for number");
+            for (int i = 1; i < 10000; i++)
+            {               
+                Random randnum = new Random();
+                r = randnum.Next(1, 377);
+                Console.WriteLine(r);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
